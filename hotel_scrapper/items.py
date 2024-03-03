be84332fba1a48c5663e924de7ea01f70ca06805
Ahmed Zahir <xahiru@gmail.com>
2024-03-03 21:03:11 +0800

    first commit

---
+++ hotel_scrapper/items.py
@@ -0,0 +1,61 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HotelScrapperItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class HotelItem(scrapy.Item):
+    name = scrapy.Field()
+    star = scrapy.Field()
+    guest_rating = scrapy.Field()
+    # reviews = scrapy.Field()
+    room_type = scrapy.Field()
+    # room_details = scrapy.Field()
+    # eminities = scrapy.Field()
+    # occupency = scrapy.Field()
+    d_price = scrapy.Field()
+    original_price = scrapy.Field()
+    # price_details = scrapy.Field()
+    conditions = scrapy.Field()
+    # url = scrapy.Field()
+
+
+class BookingReviewer(scrapy.Item):
+    hotel_name = scrapy.Field()
+    title = scrapy.Field()
+    name = scrapy.Field()
+    age = scrapy.Field()
+    date = scrapy.Field()
+    room_type = scrapy.Field()
+    guest_type = scrapy.Field()
+    trip_type = scrapy.Field()
+    length_of_stay = scrapy.Field()
+    country = scrapy.Field()
+    user_review_count = scrapy.Field()
+    #language = scrapy.Field()
+    positive_content = scrapy.Field()
+    negative_content = scrapy.Field()
+    score = scrapy.Field()
+    #tags = scrapy.Field()
+    posted_via = scrapy.Field()
+    hotel_clean = scrapy.Field()
+    hotel_comfort = scrapy.Field()
+    hotel_location = scrapy.Field()
+    hotel_clean = scrapy.Field()
+    hotel_staff = scrapy.Field()
+    hotel_services = scrapy.Field()
+    hotel_value = scrapy.Field()
+    hotel_wifi = scrapy.Field()
+    #superb = scrapy.Field()
+    #good = scrapy.Field()
+    #ok = scrapy.Field()
+    #poor = scrapy.Field()
+    #verypoor = scrapy.Field()
---
+++ hotel_scrapper/spiders/rates_spider.py
@@ -0,0 +1,105 @@
+import scrapy
+from hotel_scrapper.items import HotelItem
+# from scrapy.loader import ItemLoader
+from selenium import webdriver
+
+
+from datetime import datetime, timedelta
+checinkin = datetime.now() + timedelta(days=14)
+checkin = checinkin.strftime('%Y-%m-%d')
+checkin_year = checkin[:4]
+checkin_month = checkin[5:7]
+checkin_day = checkin[8:]
+checkout = datetime.now() + timedelta(days=15)
+checkout = checkout.strftime('%Y-%m-%d')
+checkout_year = checkout[:4]
+checkout_month = checkout[5:7]
+checkout_day = checkout[8:]
+
+class RatesSpiderSpider(scrapy.Spider):
+    name = "rates_spider"
+    allowed_domains = ['www.booking.com']
+
+    start_urls = [
+        "https://www.booking.com/searchresults.en-gb.html?ss=Maldives&ssne=&ssne_untouched=&label=gen173nr-1FCAEoggI46AdIM1gEaLQBiAEBmAEJuAEHyAEM2AEB6AEB-AELiAIBqAIDuAKcgfKuBsACAdICJDMzMDk1NWE2LTNlMGMtNDU1Ni1iYmE4LTBmNTZhMjY2NmRhNNgCBuACAQ&sid=d8f692159d780d393c7c6e9ed3d571a6&aid=304142&lang=en-gb&sb=1&src_elem=sb&src=index&dest_id=-2437894&dest_type=city&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=7853560ee74a00b3&ac_meta=GhA3ODUzNTYwZWU3NGEwMGIzIAAoATICZW46AU1AAEoAUAA%3D&checkin="+checkin+"&checkout="+checkout+"&group_adults=2&no_rooms=1&group_children=0"]
+    
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def parse(self, response):
+        self.driver.get(self.start_urls[0])
+
+        mx_pages = 20
+
+        while mx_pages > 0:
+            mx_pages = mx_pages - 1
+            try:
+
+                print(
+                    '==================================================================inside the loop===============')
+                self.driver.implicitly_wait(15)
+                # print(response.xpath('//button[@aria-label="Next page"]'))
+
+                next = self.driver.find_element_by_xpath(
+                    '//button[@aria-label="Next page"]')
+                # print(next)
+                url = self.driver.current_url
+                # print(url)
+                yield scrapy.Request(url, callback=self.parse_hotel)
+                print("page xxxxxx")
+                next.click()
+                # self.driver.implicitly_wait(10)
+            except:
+                print(
+                    '==================================================================failed or end===============')
+                break
+
+        self.driver.close()
+
+    def parse_hotel(self, response):
+        print('==================================================================Passing started===============')
+        print('response')
+        # print(response)
+
+        # print(response.xpath('//div[@data-testid="property-card"]').extract())
+        # hotel_cards = Response.xpath(
+        #     '//div[@data-testid="property-card"]')
+        hotel_titles = response.xpath(
+            '//div[@data-testid="title"]').xpath('.//text()').extract()
+        hotel = []
+        location = response.xpath(
+            '//span[@data-testid="address"]').xpath('.//text()').extract()
+
+        # some stars are 0
+        start_counts = response.xpath(
+            '//div[@data-testid="rating-squares"]')
+        prices = response.xpath(
+            '//div[@data-testid="price-and-discounted-price"]').xpath('.//span/text()').extract()
+        # SOme are unavaialable
+        # recom_filter = a[not(contains(@id, 'xx'))]
+        # recom_units = response.xpath('//div[@data-testid="recommended-units"]').xpath('.//dev/dev/dev[0]/span/text()'
+        # .xpath('.//dev/dev/dev[0]/span/text()').extract()
+        # recom_units = response.xpath(
+        #     '//div[@data-testid="recommended-units"]').xpath('.//div/').get()
+        # .xpath('.//dev/dev/dev[0]/span/text()').extract()
+
+        print("=============printing====title===")
+        for price, title, address in zip(hotel_titles, prices, location):
+            print(title)
+            print(price)
+            print(address)
+            # print(recom_units)
+            # print(len(recom_units))
+            # print(star)
+
+            hotel = HotelItem()
+
+            hotel['name'] = title
+            hotel['star'] = "star"
+            hotel['d_price'] = price
+            # hotel['room_type'] = recom_units
+            hotel['original_price'] = address
+            hotel['guest_rating'] = "none"
+
+            yield hotel
